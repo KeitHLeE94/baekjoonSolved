c824cb7937e5fc11b89f9ab7033f91be0b9529ff
KeithLee <wogus4187@naver.com>
2017-12-26 11:34:45 +0900

    Add Readme.txt

---
+++ makeone.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int dp[100001];
+
+int min(int a, int b){
+  return a > b ? b : a;
+}
+
+int main(void){
+  int input, i;
+
+  scanf("%d", &input);
+
+  dp[0] = 0;
+  dp[1] = 0;
+
+  for(i=2; i<=input; i++){
+    dp[i] = dp[i-1] + 1;
+    if(i%2 == 0){
+      dp[i] = min(dp[i], dp[i/2] + 1);
+    }
+    if(i%3 == 0){
+      dp[i] = min(dp[i], dp[i/3] + 1);
+    }
+  }
+
+  printf("%d\n", dp[input]);
+
+  return 0;
+}
