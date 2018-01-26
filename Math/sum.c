3c57f9b9288202559901611a4410a1147b4760dc
KeitHLeE94 <wogus4187@naver.com>
2018-01-26 11:07:05 +0900

    8393

---
+++ Math/sum.c
@@ -0,0 +1,19 @@
+//8393번
+
+#include <stdio.h>
+
+int main(void){
+  int n;
+  int i;
+  int sum = 0;
+
+  scanf("%d", &n);
+
+  for(i=1; i<=n; i++){
+    sum += i;
+  }
+
+  printf("%d\n", sum);
+
+  return 0;
+}
